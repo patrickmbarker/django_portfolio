f46a6f54947900318bb2bc8db68ef6b1e727366e
Patrick Barker <Patrick.Barker@ascension.org>
2024-04-03 15:36:07 -0500

    Configured SQLite for basic functionality testing

--- ASHAMON/main/urls.py
+++ ASHAMON/main/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 #These URLs represent the different views in our views.py file
+#The URLs file represents what URL address will be directed to which view.
+#Furthermore, the models.py file can be imported into the views file to generate views that reference the data models in models.py
 
 urlpatterns = [
-    path("",views.index, name="index"),
+    path("<int:id>",views.index, name="index"),
+    path("home/",views.v1, name="home"),
 ]
