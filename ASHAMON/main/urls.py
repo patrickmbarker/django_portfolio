abc58c9f4240af67c98c34a74664d46f905de4a7
Patrick Barker <Patrick.Barker@ascension.org>
2024-04-03 19:21:08 -0500

    Configured the admin login/superuser Added form for creating tables

--- ASHAMON/main/urls.py
+++ ASHAMON/main/urls.py
@@ -7,5 +7,6 @@ from . import views
 
 urlpatterns = [
     path("<int:id>",views.index, name="index"),
-    path("home/",views.v1, name="home"),
+    path("",views.home, name="home"),
+    path("create/",views.create, name="create"),
 ]
